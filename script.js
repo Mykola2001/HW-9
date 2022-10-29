b9817f4b8781eb4d72355961f9c2f96cb7c4cb86
Mykola2001 <myhalyuk0@ukr.net>
2022-10-29 08:31:03 +0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const num1 = parseFloat(prompt("First number"))
-const num2 = parseFloat(prompt("Second number"))
-const answer = num1 + num2
-const alertQuestion = alert(`The sum of ${num1} + ${num2} is ${answer}`)
-console.log(typeof answer)
+
+let resoult = (a + b < 4) ? true : false;
 
 
